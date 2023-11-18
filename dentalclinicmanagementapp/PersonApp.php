fc7e6448c3d6c2ff13f61fa5e93c891288263c62
parameswari.bala <parameswaribala@gmail.com>
2023-11-18 15:01:18 +0530

    updated

--- dentalclinicmanagementapp/PersonApp.php
+++ dentalclinicmanagementapp/PersonApp.php
@@ -8,8 +8,26 @@ use models\Address;
 use models\Doctor;
 use models\Patient;
 
-$person1= new Patient("A347847","Parameswari","Bala","","1970-12-02"
+$person1= new Patient("A347847","Parameswari23445","Bala","","1970-12-02"
     ,new Address("","","",0,""),9952032862,'');
+
+$date=$person1->getDob();
+
+$today=date("Y-m-d");
+
+
+if($date < $today)
+    echo "DOB is correct\n";
+else
+    echo "Not correct\n";
+
+$firstName=$person1->getFirstName();
+$pattern="/[A-Za-z]*/";
+if(preg_match_all($pattern,$firstName))
+    echo "Match Found";
+else
+    echo "Match Not Found";
+
 
 $person2=new Doctor("A347847","Parameswari","Bala","","1970-12-02"
     ,new Address("","","",0,""),9952032862,'349434','');
