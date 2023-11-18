bd0979f8f47cdf64224a428b89996d43730aceee
parameswari.bala <parameswaribala@gmail.com>
2023-11-18 15:11:35 +0530

    updated

--- implesystypescriptnov2023/dentalclinicmnagementapp/utility/dynamicpolymorphism.ts
+++ implesystypescriptnov2023/dentalclinicmnagementapp/utility/dynamicpolymorphism.ts
@@ -7,8 +7,23 @@ import {Report} from "../models/Report";
 //run the file using
 //npm install -g ts-node
 //ts-node dynamicpolymorphism.ts
-let personObj1=new Patient('', '', '', new Date(), [9952032862], Gender.FEMALE, new Address(), new Report());
+let personObj1=new Patient('', '', '', new Date(2024,12,2), [9952032862], Gender.FEMALE, new Address(), new Report());
 let personObj2=new Doctor('', '', '', new Date(), [9952032862], Gender.FEMALE, new Address(), '',null);
+console.log(new Date());
+console.log(personObj1.dob);
+if(personObj1.dob <new Date())
+    console.log("DOB correct");
+else
+    console.log("DOB incorrect")
+
+let pattern=new RegExp( "[a-zA-Z]*")
+
+if(pattern.test(personObj1.firstName))
+    console.log("pattern matching");
+else
+    console.log("pattern not matching");
+
+
 //runtime polymorphism
 personObj1.login();
 personObj2.login();
