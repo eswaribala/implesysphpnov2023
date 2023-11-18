39996556899f74f8fe6f23dbdfa59d3da8a0169c
parameswari.bala <parameswaribala@gmail.com>
2023-11-18 15:44:19 +0530

    updated

--- implesystypescriptnov2023/dentalclinicmnagementapp/utility/dynamicpolymorphism.ts
+++ implesystypescriptnov2023/dentalclinicmnagementapp/utility/dynamicpolymorphism.ts
@@ -3,6 +3,8 @@ import {Gender} from "../models/Person";
 import {Doctor} from "../models/Doctor";
 import {Address} from "../models/Address";
 import {Report} from "../models/Report";
+import {DOBException} from "../exceptions/DOBException";
+
 
 //run the file using
 //npm install -g ts-node
@@ -11,10 +13,22 @@ let personObj1=new Patient('', '', '', new Date(2024,12,2), [9952032862], Gender
 let personObj2=new Doctor('', '', '', new Date(), [9952032862], Gender.FEMALE, new Address(), '',null);
 console.log(new Date());
 console.log(personObj1.dob);
-if(personObj1.dob <new Date())
-    console.log("DOB correct");
-else
-    console.log("DOB incorrect")
+
+
+
+try {
+    if (personObj1.dob < new Date())
+        console.log("DOB correct");
+    else {
+        throw new DOBException('Entered Date is the future');
+    }
+}
+catch(error:unknown){
+    //mandatory
+    if (error instanceof DOBException) {
+        console.error("DOB error:", error.message);
+    }
+}
 
 let pattern=new RegExp( "[a-zA-Z]*")
 
