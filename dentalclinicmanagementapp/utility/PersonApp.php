ba03f5194867851a5c2140042aba172c2220b773
parameswari.bala <parameswaribala@gmail.com>
2023-11-18 12:24:29 +0530

    updated

--- dentalclinicmanagementapp/utility/PersonApp.php
+++ dentalclinicmanagementapp/utility/PersonApp.php
@@ -1,7 +1,9 @@
 <?php
-$person1=new models\Patient("A347847","Parameswari","Bala","","1970-12-02"
-    ,new models\Address("","","",0,""),9952032862);
+$person1=new \models\Patient("A347847","Parameswari","Bala","","1970-12-02"
+    ,new \models\Address("","","",0,""),9952032862,'');
 
 $person2=new models\Doctor("A347847","Parameswari","Bala","","1970-12-02"
     ,new models\Address("","","",0,""),9952032862,'349434','');
 
+$person1->login();
+$person2->login();
