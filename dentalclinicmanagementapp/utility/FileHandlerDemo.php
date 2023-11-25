22c756255bcf2635758a7e0caf3c5ec6a17525ea
parameswari.bala <parameswaribala@gmail.com>
2023-11-25 13:16:54 +0530

    updated

--- dentalclinicmanagementapp/utility/FileHandlerDemo.php
+++ dentalclinicmanagementapp/utility/FileHandlerDemo.php
@@ -4,3 +4,8 @@ $file=fopen($fileName,"r");
 $content=fread($file, filesize($fileName));
 echo "$content";
 fclose($file);
+
+//write content into file
+$file=fopen($fileName,"a");
+fwrite($file,"PHP,PERL,Python");
+fclose($file);
