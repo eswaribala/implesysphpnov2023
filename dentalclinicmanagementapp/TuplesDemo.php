70cda1bf26b1047ebd56c3c3d758efc0fe7c37b0
parameswari.bala <parameswaribala@gmail.com>
2023-11-25 11:25:58 +0530

    updated

--- dentalclinicmanagementapp/TuplesDemo.php
+++ dentalclinicmanagementapp/TuplesDemo.php
@@ -1,5 +1,6 @@
 <?php
 $bloodGroup=["O+","O-","A+","B+","AB+","B-"];
 $organs=["Heart","Kidney","Liver"];
+//destructuring
 list($bg,$org)=[$bloodGroup,$organs];
 
--- implesystypescriptnov2023/dentalclinicmnagementapp/utility/TupleDemo.ts
+++ implesystypescriptnov2023/dentalclinicmnagementapp/utility/TupleDemo.ts
@@ -1,3 +1,13 @@
 const BLOODGROUP: readonly [string,string,string,string,string,string]=["O+","O-","A+","B+","AB+","B-"]
 console.log(BLOODGROUP)
 //BLOODGROUP.push("R+")
+let NewBLOODGROUP:any[]=[]
+/*BLOODGROUP.forEach(bg=>{
+    NewBLOODGROUP.push(bg)
+})*/
+copyBloodGroups(BLOODGROUP)
+function copyBloodGroups(...data){
+    NewBLOODGROUP.push(data)
+}
+NewBLOODGROUP.push("R+")
+console.log(NewBLOODGROUP)
