20b6edb3c7f2304ec61ba13010f5945c014a0629
parameswari.bala <parameswaribala@gmail.com>
2023-11-25 14:51:35 +0530

    updated

--- dentalclinicmanagementapp/LambdaFunction.php
+++ dentalclinicmanagementapp/LambdaFunction.php
@@ -1,15 +1,29 @@
 <?php
 //lambda method
 //version 7.4 onwards
+//method1
 $totalBill=fn($bill1,$bill2)=> $bill1+$bill2;
 
 echo $totalBill(438658,3956936);
 
-
+//method 2
 $bills=[35,2,6,12];
 
 $totalBill=array_map(fn($data)=>$data*$data,$bills);
 print_r($totalBill);
+
+
+//method3
+
+$totalBill=function ($data){
+    $sum=0;
+    foreach($data as $d){
+        $sum+=$d;
+    }
+    return $sum;
+};
+
+echo $totalBill($bills);
 
 
 
